17e04f462dc14d5bc018b228fbac02f91381531e
Viktor7421 <40173220+Viktor7421@users.noreply.github.com>
2019-07-09 16:31:53 -0500

    Actualizacion

--- app/src/main/java/cs2901/utec/chat_mobile/LoginActivity.java
+++ app/src/main/java/cs2901/utec/chat_mobile/LoginActivity.java
@@ -1,5 +1,6 @@
 package cs2901.utec.chat_mobile;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -33,6 +34,10 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
+    public Activity getActivity(){
+        return this;
+    }
+
     public void onBtnLoginClicked(View view) {
         // 1. Getting username and password inputs from view
         EditText txtUsername = (EditText) findViewById(R.id.txtUsername);
@@ -61,6 +66,10 @@ public class LoginActivity extends AppCompatActivity {
                         String message = response.getString("message");
                         if(message.equals("Authorized")) {
                             showMessage("Authenticated");
+                            Intent intent = new Intent(getActivity(), ContactsActivity.class);
+                            intent.putExtra("user_id", response.getInt("user_id"));
+                            intent.putExtra("username", response.getString("username"));
+                            startActivity(intent);
                         }
                         else {
                             showMessage("Wrong username or password");
